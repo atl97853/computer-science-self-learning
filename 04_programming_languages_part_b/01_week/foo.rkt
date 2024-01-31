42ac77fa3e2804b371cf41c854b1280ad0f7f103
atl97853 <128220069+atl97853@users.noreply.github.com>
2024-01-31 11:14:47 -0600

    Update

--- 04_programming_languages_part_b/01_week/foo.rkt
+++ 04_programming_languages_part_b/01_week/foo.rkt
@@ -11,3 +11,20 @@
     (* x (pow1 x (- y 1)))))
 
 (pow1 3 2)
+; -----------------------------------------
+; List
+; Sum all the number in a list 
+(define (sum xs)
+    (if (null? xs)
+        0
+        (+ (car xs) (sum (cdr xs)))))
+(sum (list 1 2 3))
+
+; Append 
+(define (my-append xs ys)
+    (if (null? xs)
+    ys
+    (cons (car xs) (my-append (cdr xs) ys))))
+
+(my-append (list 1 2) (list 3 4))
+my-append
