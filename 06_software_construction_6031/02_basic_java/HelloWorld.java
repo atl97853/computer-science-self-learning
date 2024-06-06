f726e2520afa1ac8a6654ebe3ce4aba445fe262c
atl97853 <128220069+atl97853@users.noreply.github.com>
2024-06-06 16:23:26 -0600

    Update

--- 06_software_construction_6031/02_basic_java/HelloWorld.java
+++ 06_software_construction_6031/02_basic_java/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     static public void main(String[] args) {
-        System.out.println("Hello World!");
+        if ('A' < 'B') {
+            System.out.println("Hi !!!");
+        }
+        
     }
 }
