2f464fe1059f8329731b0476fa14bbeb8d791a90
atl97853 <128220069+atl97853@users.noreply.github.com>
2024-06-13 11:43:37 -0600

    Update

--- 06_software_construction_6031/02_basic_java/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    static public void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        String myText = keyboard.nextLine("Enter your name: ");
-        System.out.print(myText);
-    }
-}
