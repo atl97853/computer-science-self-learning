e11b122fa8e39904e37f0bd83ddcf6ed2e55dbe7
atl97853 <128220069+atl97853@users.noreply.github.com>
2024-06-18 12:42:43 -0600

    Update

--- 06_software_construction_6031/02_basic_java/Main.java
+++ 06_software_construction_6031/02_basic_java/Main.java
@@ -1,11 +1,65 @@
+// public class Main {
+//     static public void main(String[] args) {
+//         System.out.println(MyClass.MY_NAME);
+//         MyClass s = new MyClass("Ragnar", 00);
+//         System.out.println(s.getName());
+//         MyClass.helloWorld(s.getName());
+//     }
+// }
+
+// this example passes the compile check but no the runtime check
+// public class Main {
+//     static public void main(String[] args) {
+//         Integer[] list = new Integer[]{
+//             1,2,3,4,5
+//         };
+//         for (int i = 1; i <= 6; i++) {
+//             System.out.println(list[i - 1]);
+//         } 
+//     }
+// }
+
+
+// public class Main {
+//     static public void main(String[] args) {
+//         MyClass p = new MyClass(92045, 186);
+//         System.out.println(p.getId());
+//         System.out.println(p.getAge());
+//         p.setAge(563);
+//         System.out.println(p.getAge());
+//     }
+// }
+
+// import java.util.*;
+
+// public class Main {
+//     static public void main(String[] args) {
+//         List<String> list = new ArrayList<>(List.of("A","B"));
+//         for (String item : list) {
+//              System.out.println(item);
+//         }
+//         Set<Integer> numbers = new HashSet<>(List.of(1,2));
+//         for (Integer item : numbers) {
+//              System.out.println(item);
+//         }
+//         // List<String> list = new ArrayList<>(List.of("A","B"));
+//         // for (String item : list) {
+//         //      System.out.println(item);
+//         // }
+//     }
+// }
+
+
 public class Main {
+    public enum Month { 
+        JANUARY, FEBRUARY, MARCH, APRIL, 
+        MAY, JUNE, JULY, AUGUST, 
+        SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER;
+    }
     static public void main(String[] args) {
-        System.out.println(MyClass.MY_NAME);
-        MyClass s = new MyClass("Ragnar", 00);
-        System.out.println(s.getName());
-        MyClass.helloWorld(s.getName());
+        Month month; // initialize variable
+        month = Month.MARCH; // declare 
+        Month nov = Month.NOVEMBER; // initialize and declare 
+        System.out.println(nov);
     }
 }
-
-
-
--- 06_software_construction_6031/02_basic_java/MyClass.java
+++ 06_software_construction_6031/02_basic_java/MyClass.java
@@ -1,15 +1,54 @@
+// public class MyClass {
+//     public static final String MY_NAME = "Atl";
+//     private String name;
+//     private int id;
+//     public MyClass(String name, int id) {
+//         this.name = name;
+//         this.id = id;
+//     }
+//     public String getName() {
+//         return this.name;
+//     }
+//     public static void helloWorld(String name){
+//         System.out.println("Hello World " + name);
+//     }
+// } 
+
+// public class MyClass {
+//     private final int id;
+//     private int age;
+//     public MyClass(int id, int age) {
+//         this.id = id;
+//         this.age = age;
+//     }
+//     public int getId() {
+//         return this.id;
+//     }
+//     public void setAge(int newAge) {
+//         this.age = newAge;
+//     }
+//     public int getAge() {
+//         return this.age;
+//     }
+    
+
+// }
+
 public class MyClass {
-    public static final String MY_NAME = "Atl";
-    private String name;
-    private int id;
-    public MyClass(String name, int id) {
-        this.name = name;
-        this.id = id;
+    public static final enum Month {
+        JAN, FEB;
     }
-    public String getName() {
-        return this.name;
-    }
-    public static void helloWorld(String name){
-        System.out.println("Hello World " + name);
-    }
+    // public MyClass(int id, int age) {
+    //     this.id = id;
+    //     this.age = age;
+    // }
+    // public int getId() {
+    //     return this.id;
+    // }
+    // public void setAge(int newAge) {
+    //     this.age = newAge;
+    // }
+    // public int getAge() {
+    //     return this.age;
+    // }
 }
