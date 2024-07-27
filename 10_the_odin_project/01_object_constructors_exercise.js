eb06df9f3e2f03c9e944a1bf36139807dee95181
atl97853 <128220069+atl97853@users.noreply.github.com>
2024-07-26 19:17:53 -0600

    Update Finish Objects and Object Constructors reading, start Project: Library only js

--- 10_the_odin_project/01_object_constructors_exercise.js
+++ 10_the_odin_project/01_object_constructors_exercise.js
@@ -19,4 +19,79 @@ function Book (title, author, numberPages, statusRead) {
 }
 
 const myBook = new Book("The Hobbit", "J.R.R Tolkien", 295, "not read yet");
-console.log(myBook.info());
+// console.log(myBook.info());
+
+// Initialize constructor functions
+function Hero(name, level) {
+    this.name = name;
+    this.level = level;
+}
+
+function Warrior(name, level, weapon) {
+Hero.call(this, name, level);
+
+this.weapon = weapon;
+}
+
+function Healer(name, level, spell) {
+Hero.call(this, name, level);
+
+this.spell = spell;
+}
+  
+// Link prototypes and add prototype methods
+Object.setPrototypeOf(Warrior.prototype, Hero.prototype);
+Object.setPrototypeOf(Healer.prototype, Hero.prototype);
+
+Hero.prototype.greet = function () {
+return `${this.name} says hello.`;
+}
+
+Warrior.prototype.attack = function () {
+return `${this.name} attacks with the ${this.weapon}.`;
+}
+
+Healer.prototype.heal = function () {
+return `${this.name} casts ${this.spell}.`;
+}
+
+// Initialize individual character instances
+const hero1 = new Warrior('Bjorn', 1, 'axe');
+const hero2 = new Healer('Kanin', 1, 'cure');
+
+
+/**
+ * This is the standard and recommended way to access the prototype of an object (getter and setter):
+ * Object.getPrototypeOf(object)
+ *  Object.setPrototypeOf(object)
+ */
+
+// const myObject = {};
+// const prototype = Object.getPrototypeOf(myObject);
+// console.log(prototype); // Output: Object.prototype (assuming myObject inherits from the default prototype chain)
+
+// animal has methods
+let animal = {
+    walk() {
+        if (!this.isSleeping) {
+        console.log(`I walk`);
+        }
+    },
+sleep() {
+    this.isSleeping = true;
+}
+};
+
+let rabbit = {
+name: "White Rabbit",
+__proto__: animal
+};
+
+// modifies rabbit.isSleeping
+rabbit.sleep();
+
+console.log(rabbit.isSleeping); // true
+//   animal.sleep();
+console.log(animal.isSleeping); 
+
+
