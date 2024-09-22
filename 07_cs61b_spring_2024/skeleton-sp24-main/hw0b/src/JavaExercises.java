f4b7386e8c1387b50ef7dd545b8279e44a0b97aa
atl97853 <128220069+atl97853@users.noreply.github.com.>
2024-09-21 18:02:47 -0600

    Finish task 1: JavaExercises.java

--- 07_cs61b_spring_2024/skeleton-sp24-main/hw0b/src/JavaExercises.java
+++ 07_cs61b_spring_2024/skeleton-sp24-main/hw0b/src/JavaExercises.java
@@ -5,8 +5,8 @@ public class JavaExercises {
 
     /** Returns an array [1, 2, 3, 4, 5, 6] */
     public static int[] makeDice() {
-        // TODO: Fill in this function.
-        return null;
+        int[] arr = new int[] {1,2,3,4,5,6};
+        return arr;
     }
 
     /** Returns the order depending on the customer.
@@ -14,15 +14,24 @@ public class JavaExercises {
      *  If the customer is Erik, return ["sushi", "pasta", "avocado", "coffee"].
      *  In any other case, return an empty String[] of size 3. */
     public static String[] takeOrder(String customer) {
-        // TODO: Fill in this function.
-        return null;
+        if (customer.equals("Ergun")) {
+            String[] arr = new String[] {"beyti", "pizza", "hamburger", "tea"};
+            return arr;
+        } else if (customer.equals("Erik")) {
+            String[] arr = new String[] {"sushi", "pasta", "avocado", "coffee"};
+            return arr;
+        } else {
+            String[] arr = new String[3];
+            return arr;
+        }
     }
 
     /** Returns the positive difference between the maximum element and minimum element of the given array.
      *  Assumes array is nonempty. */
     public static int findMinMax(int[] array) {
-        // TODO: Fill in this function.
-        return 0;
+        int min = array[0];
+        int max = array[array.length - 1];
+        return max - min;
     }
 
     /**
@@ -38,8 +47,9 @@ public class JavaExercises {
     }
 
     private static List<Integer> hailstoneHelper(int x, List<Integer> list) {
-        // TODO: Fill in this function.
-        return null;
+        list.add(x);
+        if (x == 1 ) return list;
+        if (x % 2 == 0) return hailstoneHelper(x / 2, list);
+        return hailstoneHelper((x * 3) + 1, list);
     }
-
 }
