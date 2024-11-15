ea40b2a6aaca699c03bbf7cca308671c85f7559b
atl97853 <128220069+atl97853@users.noreply.github.com.>
2024-11-14 18:09:35 -0600

    Create ArrayDeque61B class and implement Deque61B interface

---
+++ 07_cs61b_spring_2024/skeleton-sp24-main/proj1b/src/ArrayDeque61B.java
@@ -0,0 +1,48 @@
+import java.util.List;
+
+public class ArrayDeque61B <T> implements Deque61B {
+    @Override
+    public void addFirst(Object x) {
+
+    }
+
+    @Override
+    public void addLast(Object x) {
+
+    }
+
+    @Override
+    public List toList() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Object removeFirst() {
+        return null;
+    }
+
+    @Override
+    public Object removeLast() {
+        return null;
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public Object getRecursive(int index) {
+        return null;
+    }
+}
